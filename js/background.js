6f3aa0f91da5f99ecd9453db301c8b37a124a9df
cdrainxv <cdrainxv@users.noreply.github.com>
2017-12-06 23:03:55 -0700

    setup updateTab to redirect to reddit page

--- js/background.js
+++ js/background.js
@@ -1,7 +1,14 @@
-var url = 'https://www.reddit.com/r/KDRAMA/5o2lld/i_created_this_website_that_streams_kdramas_in_hd/?st=jan66p4y&sh=3c953db8'
+var url =
+  'https://www.reddit.com/r/KDRAMA/5o2lld/i_created_this_website_that_streams_kdramas_in_hd/?st=jan66p4y&sh=3c953db8'
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.query({ currentWindow: true, active: true }, function(tab) {
     updateTab(tab)
   })
 })
+
+function updateTab(tab) {
+  chrome.tab.update(tab[0].id, { url: url, active: true }, function(tab) {
+    onTabUpdated(tab.id, { status: tab.status }, tab)
+  })
+}
