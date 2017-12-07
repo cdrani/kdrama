0e6c9fa5eaf20f9ccc92670335a4f4aaa8d59d2e
cdrainxv <cdrainxv@users.noreply.github.com>
2017-12-06 23:00:58 -0700

    setup browserAction to get current tab info

---
+++ js/background.js
@@ -0,0 +1,7 @@
+var url = 'https://www.reddit.com/r/KDRAMA/5o2lld/i_created_this_website_that_streams_kdramas_in_hd/?st=jan66p4y&sh=3c953db8'
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.query({ currentWindow: true, active: true }, function(tab) {
+    updateTab(tab)
+  })
+})
